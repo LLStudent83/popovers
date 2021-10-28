aff78980852ef13d57e793885449bb590beb227d
Dmitry Prigulnov <prigulnov@mail.ru>
2021-10-28 18:55:41 +0500

    Подготовил проект

---
+++ e2e/showAffiliation.test.js
@@ -0,0 +1,43 @@
+// import { fork } from 'child_process';
+
+// const puppetteer = require('puppeteer');
+
+// jest.setTimeout(30000);
+// describe('Check сard', () => {
+//   let browser = null;
+//   let page = null;
+//   let server = null;
+//   const baseUrl = 'http://localhost:9000';
+//   beforeAll(async () => {
+//     server = fork(`${__dirname}/e2e.server.js`);
+//     await new Promise((resolve, reject) => {
+//       server.on('error', reject);
+//       server.on('message', (message) => {
+//         if (message === 'ok') {
+//           resolve();
+//         }
+//       });
+//     });
+
+//     browser = await puppetteer.launch({
+//       headless: false, // show guis
+//       lowMo: 250,
+//       devtools: true, // show devTools
+//     });
+//     page = await browser.newPage();
+//   });
+//   afterAll(async () => {
+//     await browser.close();
+//     server.kill();
+//   });
+//   describe('Check сard', () => {
+//     test('проверка подсвечивания иконки карты', async () => {
+//       await page.goto(baseUrl);
+//       const input = await page.$('.input-valid-card');
+//       await input.type('4716662880185704351');
+//       const button = await page.$('.to-valid');
+//       button.click();
+//       await page.waitForSelector('.acktiv');
+//     });
+//   });
+// });
